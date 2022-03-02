42aa5109a7fda9613d7debc70550e7a6df672ac9
Neill10 <neill10@nycstudents.net>
2022-03-02 18:54:02 -0500

    this shit dont workkkkkkkkkkkk

--- src/ChessRunner.java
+++ src/ChessRunner.java
@@ -1,5 +1,9 @@
+import java.util.ArrayList;
+import java.util.Arrays;
 public class ChessRunner {
     public static void main(String[] args) {
-
+        ChessBoard board = new ChessBoard();
+        board.fillBoard();
+        board.presentBoard();
     }
 }
